9bad3f1a8a7309480a99fe41ef042c1acff04d7d
Ashar Naveed <152067974+Ashar134@users.noreply.github.com>
2025-07-26 23:52:48 +0500

    Add files via upload

---
+++ ContentRating/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ContentRating from './contentRating'; 
+
+function App() {
+  return (
+    <div>
+      <ContentRating />
+    </div>
+  );
+}
+
+export default App;
